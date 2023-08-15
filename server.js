608970fa2baaad06c9d6b32d7b3ca8875488cc34
BBIYAKYEE7 <dadalgojr@gmail.com>
2023-08-16 00:11:55 +0900

    codes

--- server.js
+++ server.js
@@ -1,34 +1,25 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const path = require('path');
-const bodyParser = require('body-parser');
+const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.json());
+app.use(cors());
 
-const PORT = process.env.PORT || 5500;
+const products = [
+    { id: 1, name: '상품1', price: 10000 },
+    { id: 2, name: '상품2', price: 20000 },
+    // ...
+];
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html')); // 수정된 부분
+app.get('/api/products/:productId', (req, res) => {
+    const productId = parseInt(req.params.productId);
+    const product = products.find(p => p.id === productId);
+    if (!product) {
+        return res.status(404).json({ error: 'Product not found' });
+    }
+    res.json(product);
 });
 
-app.post('/update-student-info', (req, res) => { // 수정된 부분
-  const { name, school, grade, room, number } = req.body;
-
-  // 여기서 학생 정보를 데이터베이스에 저장하거나 처리하는 로직을 작성합니다.
-  // 이 예시에서는 학생 정보를 그대로 응답으로 보내는 것으로 대체합니다.
-  const response = {
-    name,
-    school,
-    grade,
-    room,
-    number
-  };
-
-  // 학생 정보를 받아와서 처리한 후 응답으로 학생 정보 전송
-  res.status(200).json(response);
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}`);
 });
